a5b88aa2999af0407f4e14c342db15b3961e9c84
Cezar Rosa <cecez86@gmail.com>
2015-09-03 20:50:54 -0300

    - inserindo imagem e definindo imagem padrão da view de imagem - view para mostrar imagem - seletor de imagem (picker) - aplicando imagem selecionada na view

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +33,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(textField: UITextField) {
         mealNameLabel.text = textField.text
     }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // fecha o picker
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // referência da imagem selecionada
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // exibe imagem selecionada na view da imagem
+        photoImageView.image = selectedImage
+        
+        // fecha o picker
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        
+        // cria imagePicker
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        
+        // exibe o picker
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default text"
     }
