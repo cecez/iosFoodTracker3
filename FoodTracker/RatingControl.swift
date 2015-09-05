e509eef3a3893fdb859b8ddaa03809a07e76a438
Cezar Rosa <cecez86@gmail.com>
2015-09-05 16:20:54 -0300

    - imagens para estrelas de avaliação - definição de constante para tamanho do botão

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -18,10 +18,15 @@ class RatingControl: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
         
         for _ in 0..<5 {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-            button.backgroundColor = UIColor.redColor()
+            let button = UIButton()
+            button.setImage(emptyStarImage, forState: .Normal)
+            button.setImage(filledStarImage, forState: .Selected)
+            button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+            button.adjustsImageWhenHighlighted = false
             button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
             ratingButtons += [button]
             addSubview(button)
@@ -29,10 +34,15 @@ class RatingControl: UIView {
     }
     
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // define a largura e altura do botão para um quadrado do tamanho da altura do frame
+//        let buttonSize = Int(frame.size.height) // não funcionou
+        let buttonSize = 44
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         for (index, button) in ratingButtons.enumerate() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
             button.frame = buttonFrame
         }
     }
