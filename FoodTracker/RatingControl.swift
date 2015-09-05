b43cd5890ca397700f314dfddecf41cfc15e51bf
Cezar Rosa <cecez86@gmail.com>
2015-09-04 21:20:40 -0300

    - criação da view customizada para armazenar avaliação da refeição (estrelas)

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,45 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Cezar Castro Rosa on 04/09/15.
+//  Copyright © 2015 Cezar Castro Rosa. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    // MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+        for _ in 0..<5 {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            button.frame = buttonFrame
+        }
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+
+}
