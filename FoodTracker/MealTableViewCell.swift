597c0082eca73243b2203431b3f729a3dda7e799
Cezar Rosa <cecez86@gmail.com>
2015-09-11 21:41:08 -0300

    - inclusão da table view controller - ui para table view cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Cezar Castro Rosa on 11/09/15.
+//  Copyright © 2015 Cezar Castro Rosa. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
